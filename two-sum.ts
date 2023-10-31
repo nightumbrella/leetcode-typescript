7ab6115594c9aafccca1cd07e447e3c9bf5fd77d
NightUmbrella <githubumbrella@gmail.com>
2023-11-01 00:01:28 +0500

    add two sum first methods

--- two-sum.ts
+++ two-sum.ts
@@ -4,8 +4,6 @@
 
 // You can return the answer in any order.
 
-
-
 // Example 1:
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
@@ -17,12 +15,27 @@
 
 // Example 3:
 // Input: nums = [3,3], target = 6
-// Output: [0,1] 
-
-
+// Output: [0,1]
 
 // Constraints:
 // 2 <= nums.length <= 104
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
 // Only one valid answer exists.
+const startTime = performance.now();
+function twoSum(nums: number[], target: number): number[] | null {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return null;
+}
+const endTime = performance.now();
+const executionTime = endTime - startTime;
+
+console.log(twoSum([1, 2, 3, 4], 5));
+console.log(`Execution time →→→: ${executionTime} milliseconds ←←←`);
